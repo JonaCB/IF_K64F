7192e1ab97eacb7c46efa09cdd8d6a4ff46362e4
Jona Cabrera <jonacabrerab@hotmail.com>
2019-04-01 23:29:02 -0600

    Pos ya casi jala

--- IF_K64F_UART_HW/Sources/main.c
+++ IF_K64F_UART_HW/Sources/main.c
@@ -1,37 +1,75 @@
 #include "derivative.h" /* include peripheral declarations */
 
+//DEFINES
 #define LLAMAJONA "ATD7221755140;\r\n"
 #define CUELGAJONA "ATH\r\n"
-#define UARTSEND UART3_C2 |= (1<<6);
+#define GOTMSG "+CMTI:"
+#define READMSG "+CMGR:"
+#define UARTSEND UART3_C2 |= (1<<6)+(1<<3);
 
+//POINTERS FOR TX AND RX
 unsigned char *ptr;
 unsigned char *rx_ptr;
-unsigned char temp;
-unsigned char call[30] = {LLAMAJONA};
-unsigned char hang[10] = {CUELGAJONA};
-unsigned char data[50] = {"Hola mundo\r\n"};
-unsigned char rx_data[50];
-unsigned volatile char flag = 0;
-void delay(unsigned long tiempo_ms);
+unsigned char *msg_ptr;
+unsigned char *aux_ptr;
+unsigned char flg = 0;		//bit 0 = unrecognized chain, bit 1 = about to rx the msg number
+unsigned char aux_flg = 0b00000011;
 
+//ARRAYS
+unsigned char sendinfo[2][18] = {LLAMAJONA, CUELGAJONA};
+unsigned char rx_info[2][9] = {GOTMSG,READMSG};
+unsigned char aux_array[2];
+unsigned char rx_buffer[50];
+unsigned char msg_buffer[100];
+unsigned char read_msg[14] = {"AT+CMGR=0  \r\n"};
+unsigned char send_msg[4][25] = {"AT+CMGF=1\r\n","AT+CMGS=\"7221755140\"\r\n","Hello world",0x1A};
+//CHECAR COMO MANDAR EL ASCII
+
+//ITERATORS FOR ARRAY USED FOR PARSING
+unsigned char i;
+unsigned char j;
+unsigned char ptr_pos = 0;
+unsigned volatile char vld_opt = 0;
+
+unsigned char tmp;
+
+// FUNCTION HEADERS
+void delay(unsigned long tiempo_ms);
 void UART_init(void);
-void PORT_init(void);
-void LPTMR_init(void);
+
 
 int main(void)
 {
-	rx_ptr = rx_data;
-	ptr = data;
 	UART_init();
-	//LPTMR_init();
-	for(;;){
-		delay(1000);
-		ptr = call;
-		UARTSEND
-		delay(1000);
-		ptr = hang;
-		UARTSEND
-		delay(1000);
+	rx_ptr = rx_buffer;
+	msg_ptr = msg_buffer; 
+	/*ptr = &sendinfo[0][0];
+	UARTSEND
+	delay(10000);
+	ptr = &sendinfo[1][0];
+	UARTSEND
+	delay(1000);*/
+	ptr = &send_msg[0][0];
+	UARTSEND
+	delay(100);
+	ptr = &send_msg[1][0];
+	UARTSEND
+	delay(100);
+	ptr = &send_msg[2][0];
+	UARTSEND
+	delay(100);
+	ptr = &send_msg[3][0];
+	UARTSEND
+	delay(100);
+	for(;;)
+	{
+		if(flg&(0x4))
+		{
+			ptr = read_msg;
+			UARTSEND
+			flg&=~(1<<2);
+			vld_opt = 0;
+		}
 	}
 	return 0;
 }
@@ -49,9 +87,9 @@ void UART_init(void)
 	//ENABLE RX AND TX
 	//UART3_C2 |= (1<<5);		//RXIEN if RDRF
 	UART3_RWFIFO = 0x01;
-	UART3_C2 = (1<<6)+(1<<5)+0x0C;
+	UART3_C2 = (1<<5)+0x0C;
 	UART3_D = 0;
-	//UART3_C2 |= 0x40;
+	UART3_C2 |= 0x40;
 	
 	//PORT B16 ENABLED AS RX
 	PORTC_PCR16=0x00000300;
@@ -63,44 +101,111 @@ void UART_init(void)
 	NVICISER1|= (1<<(37%32));
 }
 
-void LPTMR_init(void)
-{
-	LPTMR0_PSR |= (1<<2)+1;
-	LPTMR0_CSR |= (1<<6) + 1;
-	LPTMR0_CMR = 5000;
-
-	//CLEAR PENDING INTERRUPTS
-	//NVICICER1 |= (1<<(58%32));
-	//ENABLE NVIC FOR INTERRUPTS
-	//NVICISER1 |= (1<<(58%32));
-}
-
 void UART3_Status_IRQHandler(void)
 {
 	if(UART3_S1 & (0x20))
 	{
-		*rx_ptr = UART3_D;
-		UART3_D = *rx_ptr;
-		rx_ptr++;
-		(void) UART3_S1;
+		if(!vld_opt)
+		{
+			//ITERATOR OF AUXILIARY ARRAY
+			for(i = 0; i < 2; i++)
+			{
+				if(!(aux_flg&(1<<i)))
+				{
+					flg |= 1;
+				}
+				if(!(flg&(0x1)))
+				{
+					if(UART3_D != rx_info[i][ptr_pos])
+					{
+						aux_flg&=~(1<<i);
+					}
+				}
+				flg &= ~(1<<0);
+			}
+			ptr_pos++;
+			if(aux_flg == 1) 
+			{
+				vld_opt = 1;
+				aux_ptr = &read_msg[8];
+				aux_flg = 3;
+				ptr_pos = 0;
+			}
+			else if (aux_flg == 2)
+			{
+				vld_opt = 2;
+				aux_flg = 3;
+				ptr_pos = 0;
+			}
+			
+			
+			if(!aux_flg)
+			{
+				aux_flg = 3;
+				ptr_pos = 0;
+			}
+			
+			//*rx_ptr = UART3_D;
+			//UART3_D = *rx_ptr;
+			(void) UART3_S1;
+		}
+		else if(flg&(0x8))
+		{
+			if(flg&(0x10))
+			{
+				*msg_ptr = UART3_D;
+				msg_ptr++;
+			}
+			if(flg&(0x10) && UART3_D == 0)
+			{
+				flg &=~(1<<3);
+				vld_opt = 0;
+			}
+			if(UART3_D == '\n')	flg|=(1<<4);
+		}
+		else if(vld_opt == 1)
+		{
+			tmp = UART3_D;
+			if(flg&(0x2))
+			{
+				*aux_ptr = UART3_D;
+				if((aux_ptr == &read_msg[10])||UART3_D == '\n')
+				{
+					flg &= ~(1<<1);
+					flg |= (1<<2);
+				}
+				aux_ptr++;
+			}
+			if(tmp == ',')
+			{
+				flg |= (1<<1);
+			}
+		}
+		else if(vld_opt == 2)
+		{
+			if(tmp >= 8) 
+			{
+				flg|=(1<<3);
+				tmp = 0;
+			}
+			if(UART3_D == '"')
+			{
+				tmp++;
+			}
+		}
 	}
 	if(UART3_S1 &(0x40))
 	{
 		(void) UART3_S1;
 		UART3_D = *ptr++;
-		if(*ptr == 0){
-			//ptr = data;
+		if(*ptr == 0)
+		{
 			UART3_C2 &= ~(1<<6);
+			UART3_C2 &= ~(1<<3);
+			//UART3_D = 0;
+			ptr = &sendinfo[0][0];
 		}
 	}
-}
-
-void LPTimer_IRQHandler(void)
-{
-	LPTMR0_CSR |= (1<<6);	
-	flag = 1;
-	//ptr = data;
-	//UART3_D = *ptr;
 }
 
 void delay(unsigned long tiempo_ms)
@@ -111,10 +216,4 @@ void delay(unsigned long tiempo_ms)
 	do{}
 	while(!(LPTMR0_CSR&0b10000000));				//Esperar la bandera TCF
 	LPTMR0_CSR |= (1<<7);
-	if(tiempo_ms > 1500)
-	{
-		ADC1_SC1A |= (1<<6);
-	}
-	//LPTMR0_CMR = 5000;
-	//LPTMR0_CSR|=(1<<7)+(1<<6)+1;		//Sobreescribir CSR para deshabilitar LPTMR y w1c de TCF
 }
